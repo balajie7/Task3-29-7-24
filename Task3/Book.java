3b02d74d01f1c9a20c9522f5c4b15350fa85b8ff
balajie7 <108389388+balajie7@users.noreply.github.com>
2024-07-31 16:14:31 +0530

    Add files via upload

---
+++ Task3/Book.java
@@ -0,0 +1,45 @@
+package Task3;
+
+public class Book {
+    int bookId;
+    String title;
+    String author;
+    boolean isAvailable;
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+//    public static void main(String[] args){
+//
+//    }
+
+}
---
+++ Task3/Library.java
@@ -0,0 +1,10 @@
+package Task3;
+
+public class Library extends Book{
+
+    
+    public static void main(String[] args){
+
+
+    }
+}
